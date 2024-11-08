7cf36c3c8e8fd34d5a2c94427d95dd6872757cfb
Matheus Bueno de Oliveira <135169698+MatheusBuenodeOliveira@users.noreply.github.com>
2024-11-08 00:19:23 +0000

    primeira etapa

--- Dollar.java
+++ Dollar.java
@@ -1,11 +1,11 @@
-    class Dollar {
-       int amount = 10;
-       Dollar(int amount) {}			
-       void times(int multiplier) {}
-    }	
+public class Dollar {
+    int amount = 10;
+    Dollar(int amount) {}			
+    void times(int multiplier) {}
+}	
 
-    public void testMultiplication() {
-       Dollar five = new Dollar(5);
-       five.times(2);
-       assertEquals(10, five.amount);
-    }
+public void testMultiplication() {
+    Dollar five = new Dollar(5);
+    five.times(2);
+    assertEquals(10, five.amount);
+}
